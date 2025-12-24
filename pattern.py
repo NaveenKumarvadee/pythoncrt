9446b8a0d5b536dffc48c25aaac9546d3806c653
Naveen kumar <vnaveenkumar415@gmail.com>
2025-12-24 11:00:15 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -10,4 +10,10 @@ for i in range(x):
     for j in range(i+1):
         print("*",end=" ")
     print("\n")
+c.
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print(j+1,end=" ")
+    print("\n")
 
